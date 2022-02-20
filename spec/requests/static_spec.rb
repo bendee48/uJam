d40cc2dfc91050bfbf6cea928d14a9ff97e74e53
Ben Daniels <benbanj@hotmail.com>
2022-02-20 18:34:07 +0000

    Adding Request Spec for StaticController
    - Adding rails-controller-testing gem
    - Adding request spec for StaticController

---
+++ spec/requests/static_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Static", type: :request do
+  describe "GET /" do
+    it "works! (now write some real specs" do
+      get root_path
+      expect(response).to render_template(:homepage)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
