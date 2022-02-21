696f40509b39bbc71cfb1028d9307384d13903ea
Ben Daniels <benbanj@hotmail.com>
2022-02-21 22:19:41 +0000

    Adding Testing & Linting Gems
    - Adding and setting up brakeman, bundler-audit & ruby-audit for security
    - Adding and setting up Rubocop for linting

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Static", type: :request do
-  describe "GET /" do
-    it "works! (now write some real specs" do
+RSpec.describe 'Static', type: :request do
+  describe 'GET /' do
+    it 'works! (now write some real specs' do
       get root_path
       expect(response).to render_template(:homepage)
       expect(response).to have_http_status(200)
