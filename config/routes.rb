929eb39dbec2bc65beef18556d356e086438f424
Ben Daniels <benbanj@hotmail.com>
2022-02-22 21:25:53 +0000

    Setting Up Devise
    - Adding devise for authentication
    - Generating files and config
    - Adding User devise routes
    - Adding authentication check to Application controller
    - Adding alerts to application.html.erb
    - Configuring Devise test helpers
    - Rejigging Static request spec to sign_in a User for test pass

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 
 Rails.application.routes.draw do
   root 'static#homepage'
+  devise_for :users
 end
--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -3,8 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Static', type: :request do
+  let(:user) { User.create!(email: 'ruth@email.com', password: 'password') }
+
   describe 'GET /' do
     it 'works! (now write some real specs' do
+      sign_in user
       get root_path
       expect(response).to render_template(:homepage)
       expect(response).to have_http_status(200)
